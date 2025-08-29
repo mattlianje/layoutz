4ec7dd4123de2a6d8551299dc05676658edb5448
Matthieu Court <matthieu.court@instacart.com>
2025-08-29 13:35:11 -0400

    feat: change bullet depth + add List[String] implicit ev

--- Layoutz.scala
+++ Layoutz.scala
@@ -194,7 +194,7 @@ package object layoutz {
       extends Element {
 
     /* Bullet styles for different nesting levels */
-    private val bulletStyles = Array("•", "◦", "▪", "‣", "⁃")
+    private val bulletStyles = Array("•", "◦", "▪")
 
     def render: String = renderAtLevel(0)
 
@@ -1468,6 +1468,17 @@ package object layoutz {
     *   a Text element containing the string
     */
   implicit def stringToText(s: String): Text = Text(s)
+
+  /** Automatic conversion from Seq[String] to Seq[Element]. Allows using
+    * string sequences directly with varargs expansion.
+    *
+    * @param strings
+    *   the sequence of strings to convert
+    * @return
+    *   a sequence of Text elements
+    */
+  implicit def stringSeqToElementSeq(strings: Seq[String]): Seq[Element] = 
+    strings.map(Text(_))
 
   /** APP FRAMEWORK
     */
