1a12f7918fb943447f8a40165577baf65d9e7002
Matthieu Court <matthieu.court@protonmail.com>
2025-09-04 22:25:37 -0400

    Update Demo.scala

--- examples/Demo.scala
+++ examples/Demo.scala
@@ -1,50 +1,44 @@
 import layoutz._
 
 object ReadmeDemo {
-  /* Define layouts */
-  val tableElement = table(Border.Round)(
-    headers = Seq("Name", "Role", "Status"),
-    rows = Seq(
-      Seq("Alice", "Engineer", "Online"),
-      Seq("Bob", "Designer", "Offline"),
-      Seq("Eve", "QA", "Away"),
-      /* Nest and compose elements */
-      Seq(
-        ul("Gegard", ul("Mousasi", ul("was a BAD man"))),
-        "Fighter",
-        "Nasty"
-      )
-    )
+/* Define layouts */
+val t = table(Border.Round)(
+  Seq("Name", "Role", "Status"),
+  Seq(
+    Seq("Alice", "Engineer", "Online"),
+    Seq("Eve", "QA", "Away"),
+    Seq(ul("Gegard", ul("Mousasi", ul("was a BAD man"))), "Fighter", "Nasty")
   )
+)
 
-  /* Combine them */
-  val demo = layout(
-    center(row("Layoutz", underline("ˆ")("DEMO"))),
+/* Nest, compose, combine them */
+val d = layout(
+  center(row("Layoutz", underline("ˆ")("DEMO"))),
+  row(
+    statusCard("Users", "1.2K"),
+    statusCard(Border.Double)("API", "UP"),
+    statusCard(Border.Thick)("CPU", "23%"),
+    t,
+    section("Pugilists")(
+      kv("Kazushi" -> "Sakuraba", "Jet" -> "Li", "Rory" -> "MacDonald")
+    )
+  ),
+  margin("[Scala!]")(
     row(
-      statusCard(Border.Thick)("API", "UP"),
-      statusCard("Users", "1.2K"),
-      statusCard(Border.Double)("CPU", "23%"),
-      tableElement,
-      section("Pugilists")(
-        kv("Kazushi" -> "Sakuraba", "Jet" -> "Li", "Rory" -> "MacDonald")
-      )
-    ),
-    margin("[Scala!]")(
-      row(
-        box("Deploy Status")(
-          layout(
-            inlineBar("Build", 1.0),
-            inlineBar("Test", 0.8),
-            inlineBar("Deploy", 0.3)
-          )
-        ),
-        tree("📁 Project")(
-          tree("src")(tree("main.scala"), tree("api.scala"))
+      box("Deploy Status")(
+        layout(
+          inlineBar("Build", 1.0),
+          inlineBar("Test", 0.8),
+          inlineBar("Deploy", 0.3)
         )
+      ),
+      tree("📁 Project")(
+        tree("src")(tree("main.scala"), tree("api.scala"))
       )
     )
   )
+)
 
-  /* Get pretty strings w/ .render */
-  println(demo.render)
+/* Get pretty strings w/ .render */
+println(d.render)
 }
